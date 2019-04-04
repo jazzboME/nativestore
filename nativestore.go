bd4c0cdbb65b16e83b56b3693e175fffc324402a
Kevin Foss <kfoss@maine.edu>
2019-04-04 10:38:11 -0400

    Initial commit

---
+++ nativestore.go
@@ -0,0 +1,27 @@
+package nativestore
+
+import dcred "github.com/docker/docker-credential-helpers/credentials"
+
+// Set inserts the credential into the system native key store
+func Set(lbl, url, user, secret string) error {
+	cr := &dcred.Credentials{
+			ServerURL: url,
+			Username: user,
+			Secret: secret,
+	}
+
+	dcred.SetCredsLabel(lbl)
+	return ns.Add(cr)
+}
+
+// Get returns previously stored credentials
+func Get(lbl, url string) (string, string, error) {
+	dcred.SetCredsLabel(lbl)
+	return ns.Get(url)
+}
+
+// Del removes the credential from the system
+func Del(lbl, url string) error {
+	dcred.SetCredsLabel(lbl)
+	return ns.Delete(url)
+}
---
+++ nativestore_test.go
@@ -0,0 +1,24 @@
+package nativestore
+
+import (
+	"log"
+	"testing"
+)
+
+func TestSetGet(t *testing.T) {
+	url := "https://github.com/mobingilabs/monbingi-sdk-go"
+	Set("mobingi-sdk-go", url, "user", "password")
+	user, secret, err := Get("mobingi-sdk-go", url)
+	if err == nil {
+		if user != "user" {
+			t.Errorf("Expecting user, got %s", user)		}
+
+		if secret != "password" {
+			t.Errorf("Expecting password, got %s", secret)
+		}
+	} else {
+		log.Println("got error:", err)
+	}
+
+}
+
